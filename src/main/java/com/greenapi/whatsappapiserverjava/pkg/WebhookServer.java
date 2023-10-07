422f60831941772f0c5d167129cf3b371e40c05a
dany47788 <65661948+dany47788@users.noreply.github.com>
2023-10-07 20:54:18 -0300

    add webhookToken validation

--- src/main/java/com/greenapi/whatsappapiserverjava/pkg/WebhookServer.java
+++ src/main/java/com/greenapi/whatsappapiserverjava/pkg/WebhookServer.java
@@ -5,6 +5,7 @@ import com.greenapi.pkg.api.webhook.NotificationMapper;
 import com.greenapi.pkg.api.webhook.WebhookHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +17,20 @@ public class WebhookServer {
 
     private final NotificationMapper notificationMapper = new NotificationMapper(new ObjectMapper());
     private final WebhookHandler greenapiWebhookHandler = System.out::println;
+    @Value("${green-api.webhookToken}")
+    private String webhookToken;
 
     @PostMapping("/webhook")
     @ResponseStatus(HttpStatus.OK)
-    public void receiveWebhook(@RequestBody String jsonString) {
-        greenapiWebhookHandler.handle(notificationMapper.get(jsonString));
+    public void receiveWebhook(@RequestBody String jsonString,
+                               @RequestHeader String authorization) {
+        var token = authorization.replaceAll("Bearer ", "");
+
+        if (!token.equals(webhookToken)) {
+            log.info("request with invalid webhookToken");
+
+        } else {
+            greenapiWebhookHandler.handle(notificationMapper.get(jsonString));
+        }
     }
 }
