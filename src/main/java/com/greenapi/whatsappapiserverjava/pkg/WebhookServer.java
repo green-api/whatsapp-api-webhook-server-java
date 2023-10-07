8b72ca7046582f28d5b466b98f23804689f8bae0
dany47788 <65661948+dany47788@users.noreply.github.com>
2023-10-06 21:27:29 -0300

    add new hook mapper

--- src/main/java/com/greenapi/whatsappapiserverjava/pkg/WebhookServer.java
+++ src/main/java/com/greenapi/whatsappapiserverjava/pkg/WebhookServer.java
@@ -1,7 +1,6 @@
 package com.greenapi.whatsappapiserverjava.pkg;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greenapi.pkg.api.webhook.NotificationMapper;
 import com.greenapi.pkg.api.webhook.WebhookHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class WebhookServer {
 
-    private final NotificationMapper notificationMapper = new NotificationMapper(new ObjectMapper());
+    private final com.greenapi.whatsappapiserverjava.pkg.NotificationMapper notificationMapper = new NotificationMapper(new ObjectMapper());
     private final WebhookHandler greenapiWebhookHandler = System.out::println;
 
     @PostMapping("/webhook")
--- src/main/java/com/greenapi/whatsappapiserverjava/pkg/models/notifications/Notification.java
+++ src/main/java/com/greenapi/whatsappapiserverjava/pkg/models/notifications/Notification.java
@@ -1,5 +1,6 @@
 package com.greenapi.whatsappapiserverjava.pkg.models.notifications;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Notification {
     private Integer receiptId;
     private NotificationBody body;
