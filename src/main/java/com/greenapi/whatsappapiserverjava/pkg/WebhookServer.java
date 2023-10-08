bf561dbdc6b9bd62e8a6fca204df950f0357c6cb
dany47788 <65661948+dany47788@users.noreply.github.com>
2023-10-07 21:33:34 -0300

    fix header

--- src/main/java/com/greenapi/whatsappapiserverjava/pkg/WebhookServer.java
+++ src/main/java/com/greenapi/whatsappapiserverjava/pkg/WebhookServer.java
@@ -25,7 +25,7 @@ public class WebhookServer {
     public void receiveWebhook(@RequestBody String jsonString,
                                @RequestHeader(required = false) String Authorization) {
 
-        if (Authorization == null ||
+        if (webhookToken == null ||
             Authorization.replaceAll("Bearer ", "").equals(webhookToken)) {
 
             greenapiWebhookHandler.handle(notificationMapper.get(jsonString));
